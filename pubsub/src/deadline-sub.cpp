7c395bf759dc875f021afbefd3fa344e7f2298e9
Adam Aposhian <aposhian.dev@gmail.com>
2022-03-28 09:31:22 -0600

    initial cmake package with deadline-sub

---
+++ pubsub/src/deadline-sub.cpp
@@ -0,0 +1,34 @@
+#include <stdint.h>
+
+#include <iostream>
+
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/int32.hpp>
+
+using namespace std::literals::chrono_literals;  // NOLINT
+
+int main()
+{
+    auto node = std::make_shared<rclcpp::Node>(nullptr);
+
+    int32_t value;
+
+    rclcpp::SubscriptionOptions options;
+    options.event_callbacks.deadline_callback = [&](auto & /*info*/) {
+        std::cout << "Deadline exceeded!\n";
+    };
+
+    auto sub = node->create_subscription<std_msgs::msg::Int32>(
+        "boring_int",
+        rclcpp::QoS(1)
+            .reliability(rclcpp::ReliabilityPolicy::BestEffort)
+            .deadline(rclcpp::Duration(1s)),
+        [&](std_msgs::msg::Int32::SharedPtr msg) {
+            value = msg->data;
+        },
+        options
+    );
+
+    rclcpp::spin(node);
+    return 0;
+}
